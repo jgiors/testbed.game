4448a924af9bc532708ce53bb0d0ac67d7c59d4e
jgiors <jgiors@yahoo.com>
2024-09-05 00:55:46 -0700

    fixed issue #8 added try/catch block to main()

--- main.cpp
+++ main.cpp
@@ -22,34 +22,42 @@ int main(int argc, const char* argv[])
 {
     int result = 0;
 
-    std::ofstream logFile;
-    app::initLogger(logFilename, logFile);
+    try {
+        std::ofstream logFile;
+        app::initLogger(logFilename, logFile);
 
-    json config;
+        json config;
 
-    {
-        std::ifstream configFile("config.json");
-        config = json::parse(configFile);
-    }
-
-    i3::Prng seedPrng;    
-
-    sf::RenderWindow window(sf::VideoMode(config["display"]["width"], config["display"]["height"]), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            std::ifstream configFile("config.json");
+            config = json::parse(configFile);
         }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+        i3::Prng seedPrng;    
+
+        sf::RenderWindow window(sf::VideoMode(config["display"]["width"], config["display"]["height"]), "SFML works!");
+        sf::CircleShape shape(100.f);
+        shape.setFillColor(sf::Color::Green);
+
+        while (window.isOpen())
+        {
+            sf::Event event;
+            while (window.pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed)
+                    window.close();
+            }
+
+            window.clear();
+            window.draw(shape);
+            window.display();
+        }
+    }
+    catch(std::exception &ex) {
+        i3logErr << "Exception caught in main()!:\n    " << ex.what() << "\n    Exiting!\n";
+    }
+    catch(...) {
+        i3logErr << "Unexpected exception (not derived from std::exception) caught in main! Exiting\n";
     }
 
     return result;
