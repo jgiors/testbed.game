07bc9748948faf2ff1de556caeea3636c98ad50a
jgiors <jgiors@yahoo.com>
2024-10-19 01:28:14 -0700

    started to implement engine::Cell, but having second thoughts...

--- Cell.h
+++ Cell.h
@@ -2,4 +2,13 @@
 #define ENGINE_CELL_H
 ///@file
 
+#include <vector>
+
+namespace engine {
+    ///Cell, which is a recursive set of child cells, though I'm not rethinking that...
+    struct Cell {
+        std::vector<std::vector<Cell>> children;
+    }
+}
+
 #endif //ENGINE_CELL_H
