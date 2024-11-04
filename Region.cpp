fc8acac7cd35fa07c9d0bf4cd0d4d08de5d84bc6
jgiors <jgiors@yahoo.com>
2024-11-03 23:55:08 -0800

    added Region class stub

---
+++ Region.cpp
@@ -0,0 +1,3 @@
+///@file
+#include "precomp.h"
+#include "Region.h"
---
+++ Region.h
@@ -0,0 +1,11 @@
+#ifndef REGION_H
+#define REGION_H
+///@file
+
+namespace engine {
+    class Region {
+        ///@todo
+    };
+}
+
+#endif //REGION_H
