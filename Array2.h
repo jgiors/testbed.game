fadf4144d07b8dcea122c67c4e05277e6408de10
jgiors <jgiors@yahoo.com>
2024-11-10 11:57:08 -0800

    WIP

--- Array2.h
+++ Array2.h
@@ -1,6 +1,17 @@
 #ifndef ARRAY2_H
 #define ARRAY2_H
 
+namespace engine {
+    ///Limited 2-dimensional bounds-checked array class. Elements are allocated at construction to
+    ///support maximum dimensions. Dimensions may be adjusted after construction.
+    template <typename T>
+    class LimitedArray2 {
+        public:
 
+        private:
+            std::vector<T> elements;
+            
+    };
+} //namespace engine
 
 #endif //ARRAY2_H
