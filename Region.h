824aae11fae73010d951b1463b288a21f64acabb
jgiors <jgiors@yahoo.com>
2024-11-25 01:45:43 -0800

    changed Region to a struct

--- Region.h
+++ Region.h
@@ -8,30 +8,12 @@
 #include "Cell.h"
 
 namespace engine {
-    ///
-    class Region {
-        public:
-            Region(Region *pParent);
-            Region() = delete;
-            Region(Region &) = delete;
-
-            LimitedArray2<Cell>* pCells() { return _pCells; }
-            const LimitedArray2<Cell>* pCells() const { return _pCells; }
-            void pCells(LimitedArray2<Cell> *p) { _pCells = p; }
-
-            std::vector<Region&>& children() { return _children; }
-            const std::vector<Region&>& children() const { return _children; }
-
-            Region* parent() { return _pParent; }
-            const Region* parent() const { return _pParent; }
-
-            // size_t widthInParent;
-            // size_t heightInParent;
-
-        private:
-            LimitedArray2<Cell> *_pCells;
-            std::vector<Region&> _children;
-            Region *_pParent;
+    ///Regions are essentially game rooms. Regions are recursive (contain other, smaller regions).
+    struct Region {
+            LimitedArray2<Cell> *pCells;                    ///This region's cell array.
+            std::vector<std::unique_ptr<Region>> children;  ///Children of this region.
+            sf::Rect<unsigned> extentInParent;              ///Bounding rect in parent region.
+            Region *pParent;                                ///Parent region or NULL for root.
     };
 } //namespace engine
 
